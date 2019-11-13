345224fd83478fa97c19f37a6ba5e1db3cd8b8e4
Craig Graham <cgraham@vmox.com>
2019-11-13 10:41:43 -0500

    adding twitter

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ctnp!');
+    expect(page.getTitleText()).toEqual('Welcome to Change the Narrative Project!');
   });
 
   afterEach(async () => {
